cf4d179bc6af157b63c7c53c8b59554d87454b1a
Patidifusa <zs20006765@estudiantes.uv.mx>
2022-12-17 22:34:29 -0600

    update #4

---
+++ src/app/funcion-lineal/funcion-lineal.ts
@@ -0,0 +1,10 @@
+export function funcionlineal(x : number[], a : number, b : number){
+    y : Number[];
+    var numDatos;
+    numDatos = x.length;
+
+    for (var i = 0; i<numDatos; i++){
+        y = a * x + b;
+    }
+    
+}
